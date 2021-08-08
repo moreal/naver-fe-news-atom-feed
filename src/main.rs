00ba2d6b9a48df098e4d32aaab63b3d2d0712152
Moreal <dev.moreal@gmail.com>
2021-08-09 04:37:36 +0900

    feat: use updated datetime from file metadata

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{fs::{self, File}, io};
+use std::{fs::{self, File}, io, time::UNIX_EPOCH};
 use comrak::{parse_document, format_html, Arena, ComrakOptions};
-use atom_syndication::{Content, Entry, EntryBuilder, FeedBuilder, Text};
+use atom_syndication::{Content, Entry, EntryBuilder, FeedBuilder, FixedDateTime, Text};
+use chrono::{FixedOffset, TimeZone};
 
 const FENEWS_PATH: &str = "./fe-news/issues";
 
@@ -19,19 +20,36 @@ fn render_markdown(content: &str) -> io::Result<String> {
 }
 
 fn main() -> io::Result<()> {
-  let news_paths = fs::read_dir(FENEWS_PATH)?
-    .map(|res| res.map(|e| e.path()))
+  let news_entries = fs::read_dir(FENEWS_PATH)?
+    .map(|res| res.map(|e| (e.path(), e.metadata().unwrap())))
     .collect::<Result<Vec<_>, io::Error>>()?;
 
   let feed_file = File::create("index.xml")?;
   let mut feed_builder = FeedBuilder::default().title("fe-news").to_owned();
   let mut entries: Vec<Entry> = vec![];
-  for path in news_paths {
+  let mut latest_updated: Option<FixedDateTime> = None;
+  for (path, metadata) in news_entries {
     let raw_content = fs::read(path.to_owned())?;
     let markdown_content = String::from_utf8(raw_content).unwrap();
     let rendered = render_markdown(&markdown_content)?;
 
     let title = Text::plain(path.to_str().to_owned().unwrap());
+    let updated: FixedDateTime = match metadata.modified()?.duration_since(UNIX_EPOCH) {
+      Ok(duration) => FixedOffset::east(0).timestamp(duration.as_secs() as i64, duration.subsec_nanos()),
+      Err(_) => {
+        return Err(io::Error::new(io::ErrorKind::Other, "error"));
+      }
+    };
+
+    if let Some(latest) = latest_updated {
+      match latest.cmp(&updated) {
+        std::cmp::Ordering::Greater => latest_updated = Some(updated),
+        _ => ()
+      };
+    } else {
+      latest_updated = Some(updated);
+    }
+
     let mut content = Content::default();
     content.set_content_type("xhtml".to_owned());
     content.set_value(rendered);
@@ -39,13 +57,17 @@ fn main() -> io::Result<()> {
     let entry = EntryBuilder::default()
       .title(title)
       .content(content)
+      .updated(updated)
       .build();
 
     entries.push(entry);
   };
 
-  feed_builder.entries(entries);
-  feed_builder.build().write_to(feed_file);
+  feed_builder
+    .entries(entries)
+    .updated(latest_updated.unwrap())
+    .build()
+    .write_to(feed_file);
 
   Ok(())
 }
